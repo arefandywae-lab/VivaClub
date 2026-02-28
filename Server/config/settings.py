2ca4de5f9c76cd804d4862e3efc4a2f9bfcdceb7
Arefandy <Arefandywae@gmail.com>
2026-02-28 22:16:53 +0700

    fix(redis): dynamically inject password into REDIS_URL if missing

--- Server/config/settings.py
+++ Server/config/settings.py
@@ -141,6 +141,12 @@ else:
 # Caches & Redis
 redis_url = os.environ.get('REDIS_URL', 'redis://:viva-redis-pass@redis:6379/1')
 
+redis_password = os.environ.get('REDIS_PASSWORD')
+if redis_password and 'redis://:' not in redis_url and '@' not in redis_url:
+    redis_url = redis_url.replace('redis://', f'redis://:{redis_password}@')
+
+os.environ['REDIS_URL'] = redis_url
+
 CACHES = {
     'default': env.cache('REDIS_URL', default=redis_url),
 }
