b78da4bac26db619acb507ec745de4aed38c1e93
Arefandy <Arefandywae@gmail.com>
2026-04-25 21:55:58 +0700

    Fix: Add missing APIView import in clinical views

--- Server/apps/clinical/views.py
+++ Server/apps/clinical/views.py
@@ -1,5 +1,6 @@
 from django.db import transaction, models
 from rest_framework import viewsets, permissions, status, filters, serializers
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.utils import timezone
